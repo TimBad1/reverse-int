aab9366ce536a87ba90ac7b0e81839da90e48598
Тим Бад <timurbadalbaev19@gmail.com>
2022-01-16 21:59:10 +0300

    feat: strange task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    str = String(Math.abs(n));
+    strReverse = '';
+    
+    for(let i = str.length -1; i >= 0; i--) {
+        strReverse += str[i];
+    }
+      
+    return +strReverse;
 }
